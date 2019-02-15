c5265e34851ea5efb0d33d1425d2c2c9853a668e
100G <100G@DESKTOP-2HTK3IR>
2019-02-16 03:57:19 +0900

    java

---
+++ java_project/java_project/src/_package/Q2_0212.java
@@ -0,0 +1,25 @@
+package _package;
+
+import java.util.*;
+
+public class Q2_0212 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int j=0,sum=0;
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(j)==s.charAt(i)) {
+				sum++;
+			}
+			else {
+				System.out.print(s.charAt(j)+""+sum);
+				j=i;
+				sum=1;
+				}
+			if(i==s.length()-1)System.out.print(s.charAt(j)+""+sum);
+			}
+	}
+
+}
