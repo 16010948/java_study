cbe5e70bddd80d3c71c31c2044f21795ad3859ea
16010948 <bjyoung0@gmail.com>
2019-03-11 22:10:19 +0900

    Hw13

---
+++ project/src/project/Hw13.java
@@ -0,0 +1,36 @@
+package project;
+
+public class Hw13 {
+	
+	static boolean stopped=false;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Thread th1=new Thread5();
+		
+		th1.start();
+		
+		try {
+			Thread.sleep(6*1000);
+		}catch(Exception e) {}
+		stopped=true;
+		th1.interrupt();
+		System.out.println("stopped");
+	}
+
+}
+
+class Thread5 extends Thread{
+	public void run() {
+		for(int i=0; !Hw13.stopped;i++) {
+			System.out.println(i);
+			/*if(Thread.interrupted()) {
+				break;
+			}*/
+			try {
+				Thread.sleep(3*1000);
+			}catch(Exception e) {}
+		}
+	}
+}
